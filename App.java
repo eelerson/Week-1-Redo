b37b97a3e5b91754544efb0bdcca5624db16d52b
eelerson <74839765+eelerson@users.noreply.github.com>
2021-06-29 07:12:27 -0700

    Add files via upload
    redone

---
+++ App.java
@@ -0,0 +1,14 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	double moneyInWallet = 60.25;
+	double costOfGroceries = 41.96;
+	double remainingMoney = moneyInWallet - costOfGroceries;
+		System.out.println("I have " + moneyInWallet + " in my wallet.");
+		System.out.println("My groceries cost " + costOfGroceries + ".");
+		System.out.println("Now I have " + remainingMoney + " left.");
+	}
+
+}
